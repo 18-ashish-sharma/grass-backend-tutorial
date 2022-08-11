0d7fd67e316ba669c9dad64d6dadcf798d53b5f6
Your Name <hudsasharma10@gmail.com>
2022-08-11 08:38:11 +0530

    adding day3

---
+++ day4-JS/main.js
@@ -0,0 +1,29 @@
+const car = {
+    name: 'hyundai',
+    model: 2019,
+};
+// ad();
+console.log(car.name);
+
+function ad () {
+    console.log('hello');
+};
+
+// const car
+// funtion add
+// car = {}
+setTimeout(() => {
+    console.log('Timer for 4 sec')
+}, 4000);
+// hoisting: https://www.w3schools.com/js/js_hoisting.asp
+// let/var/const:
+// event loop
+// async and sync funtions
+
+setTimeout(() => {
+    console.log('Timer for 2 sec')
+}, 2000);
+console.log('world');
+console.log('world');
+console.log('world');
+console.log('world');
