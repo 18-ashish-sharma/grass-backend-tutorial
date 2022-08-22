757b8ca3c73b0ce98e896ea2fe3e1622ca55c5d1
Your Name <hudsasharma10@gmail.com>
2022-08-22 08:43:35 +0530

    adding day9

---
+++ day9-mongoDB/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 8000;
+const MONGO_DB_URL = 'mongodb+srv://user:1234@cluster0.vj9s5df.mongodb.net/?retryWrites=true&w=majority'
+const connectToDB = async () => {
+    mongoose.connect(MONGO_DB_URL,
+      err => {
+          if(err) throw err;
+          console.log('connected to MongoDB Database')
+      });
+  }
+connectToDB();
+
+app.listen(PORT, function() {
+    console.log('Server running on port', PORT);
+});
+
+
+
